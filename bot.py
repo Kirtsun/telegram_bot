dfa293297c81c5b2cace149b2dbd7b858d171e05
Kyrtsun Oleh <olegkirtsun@gmail.com>
2023-04-27 22:54:44 +0300

    async

--- bot.py
+++ bot.py
@@ -1,36 +1,39 @@
-import telebot
-import time
-from telebot import types
+from aiogram import Bot, Dispatcher, executor, types
 import requests
 import os
-
 from dotenv import load_dotenv
 
 load_dotenv()
 
-key = os.getenv('API')
+bot = Bot(os.getenv('API'))
+dp = Dispatcher(bot)
 
-bot = telebot.TeleBot(os.getenv('API'))
 
-
-@bot.message_handler(commands=['start'])
-def start(massage):
+@dp.message_handler(commands=['start'])
+async def start(massage: types.Message):
     murkup = types.ReplyKeyboardMarkup()
     btn1 = types.KeyboardButton('/pars')
     btn2 = types.KeyboardButton('/currency')
     murkup.row(btn1, btn2)
-    bot.send_message(massage.chat.id, f'{massage.from_user.first_name}, Привет! Это тестовый бот, он пока что ничего не умеет кроме этого!'
-                     f'но он еще обучается! Думаю в скором времени он будет уметь гораздо больше.', reply_markup=murkup)
+    await massage.answer(f'{massage.from_user.first_name}, Привет! Это тестовый бот, он пока что ничего'
+                                      f' не умеет кроме этого! Но он еще обучается! Думаю в скором времени он будет '
+                                      f'уметь гораздо больше.', reply_markup=murkup)
 
 
-@bot.message_handler(commands=['pars'])
-def pars(massage):
+@dp.message_handler(commands=['pars'])
+async def pars(massage: types.Message):
+    murkup = types.ReplyKeyboardMarkup()
+    btn1 = types.KeyboardButton('/currency')
+    murkup.row(btn1)
     file = open('./photo.jpeg', 'rb')
-    bot.send_photo(massage.chat.id, file)
+    await massage.answer_photo(file, reply_markup=murkup)
 
 
-@bot.message_handler(commands=['currency'])
-def currency(massage):
+@dp.message_handler(commands=['currency'])
+async def currency(massage: types.Message):
+    murkup = types.ReplyKeyboardMarkup()
+    btn1 = types.KeyboardButton('/pars')
+    murkup.row(btn1)
     USD = 840
     EUR = 978
     r = requests.get(url='https://api.monobank.ua/bank/currency')
@@ -43,11 +46,13 @@ def currency(massage):
             elif i['currencyCodeA'] == EUR and i['currencyCodeB'] == 980:
                 buy_eur = i['rateBuy']
                 sell_eur = i['rateSell']
-        bot.send_message(massage.chat.id, f'Курс Валют на сегодня.\n\nUSD 🇺🇸 Покупка: {buy_usd} - Продажа {sell_usd}\n\n'
-                                          f'EUR 🇪🇺 Покупка: {buy_eur} - Продажа {sell_eur}')
+        await massage.answer(f'Курс валют на сегодня согласно данным MonoBank.\n\nUSD 🇺🇸'
+                                          f' Покупка: {buy_usd} - Продажа {sell_usd}\n\n'
+                                          f'EUR 🇪🇺 Покупка: {buy_eur} - Продажа {sell_eur}', reply_markup=murkup)
     else:
-        bot.send_message(massage.chat.id, f'Что-то пошло не так, попробуй позже')
+        await massage.answer(f'Что-то пошло не так, попробуй через 2-3 минуточки, API MonoBank не дает'
+                                          f' часто обновлять запрос. Спасибо ☺️')
 
 
 if __name__ == '__main__':
-    bot.polling(none_stop=True)
+    executor.start_polling(dp)
