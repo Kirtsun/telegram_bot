2a4f1bf09b1be968beb42a35fb909a7161cebd95
Kyrtsun Oleh <olegkirtsun@gmail.com>
2023-04-28 20:10:29 +0300

    изменения по отслеживанию текста от пользователя + регистрация команд

--- bot.py
+++ bot.py
@@ -5,14 +5,10 @@ from pars import get_currency
 
 load_dotenv()
 
-bot = Bot(os.getenv('API'))
-dp = Dispatcher(bot)
 
-
-@dp.message_handler(commands=['start'])
 async def start(massage: types.Message):
     murkup = types.ReplyKeyboardMarkup()
-    btn1 = types.KeyboardButton('/pars')
+    btn1 = types.KeyboardButton('Очень интересная функция')
     btn2 = types.KeyboardButton('Курс валют')
     murkup.row(btn1, btn2)
     await massage.answer(f'{massage.from_user.first_name}, Привет! Это тестовый бот, он пока что ничего'
@@ -20,29 +16,42 @@ async def start(massage: types.Message):
                                       f'уметь гораздо больше.', reply_markup=murkup)
 
 
-@dp.message_handler(commands=['pars'])
 async def pars(massage: types.Message):
     murkup = types.ReplyKeyboardMarkup()
-    btn1 = types.KeyboardButton('/currency')
+    btn1 = types.KeyboardButton('Курс валют')
     murkup.row(btn1)
     file = open('./photo.jpeg', 'rb')
-    await massage.answer_photo(file, reply_markup=murkup)
+    await massage.answer_photo(file, caption='А что ты думал, так быстро все будет? Нифига, я ток учу это все!',
+                               reply_markup=murkup)
 
 
-@dp.message_handler(commands=['currency'])
 async def currency(massage: types.Message):
-
     murkup = types.ReplyKeyboardMarkup()
-    btn1 = types.KeyboardButton('/pars')
+    btn1 = types.KeyboardButton('Очень интересная функция')
     murkup.row(btn1)
     res = get_currency()
     if res is not False:
         await massage.answer(f'Курс валют на сегодня согласно данным MonoBank.\n\nUSD 🇺🇸'
                                               f' Покупка: {res["USD"][0]} - Продажа {res["USD"][1]}\n\n'
-                                              f'EUR 🇪🇺 Покупка: {res["EUR"][0]} - Продажа {res["EUR"][1]}', reply_markup=murkup)
+                                              f'EUR 🇪🇺 Покупка: {res["EUR"][0]} - Продажа {res["EUR"][1]}',
+                             reply_markup=murkup)
     else:
         await massage.answer(f'Что-то пошло не так, попробуй через 2-3 минуточки. Спасибо ☺️')
 
 
+async def handler_command(massage: types.Message):
+    text = massage.text.lower().lower()
+    if text == 'курс валют':
+        await currency(massage)
+    elif text == 'очень интересная функция':
+        await pars(massage)
+    else:
+        await massage.answer(f'Что не то ввел фраерок, давай ка еще разок!')
+
+
 if __name__ == '__main__':
+    dp = Dispatcher(Bot(os.getenv('API')))
+    dp.register_message_handler(start, commands=['start'])
+    dp.register_message_handler(pars, commands=['pars'])
+    dp.register_message_handler(handler_command)
     executor.start_polling(dp)
